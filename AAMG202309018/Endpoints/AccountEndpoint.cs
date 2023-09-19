787dc216adb5f49d069daa7c232897d864e3e71b
adonismolinao3 <adonismolinao3@outlook.com>
2023-09-18 23:21:57 -0600

    tercer commit

---
+++ AAMG202309018/Endpoints/AccountEndpoint.cs
@@ -0,0 +1,37 @@
+﻿using AAMG202309018.Auth;
+namespace AAMG202309018.Endpoints
+{
+    public static class AccountEndpoint
+    {
+         static  List<object> data = new List<object>();
+
+        public static void AddAccountEndpoints(this WebApplication app)
+        {
+
+            app.MapPost("/account/login", (string login, string password, IJwtAuthenticationService authService) =>
+            {
+                if (login == "admin" && password == "12345")
+                {
+                    var token = authService.Authenticate(login);
+                    return Results.Ok(token);
+                }
+                else
+                {
+                    return Results.Unauthorized();
+                }
+            });
+
+            app.MapPost("/account/logout", (HttpContext context, IJwtAuthenticationService authService) =>
+            {
+                var token = context.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+                if (data.Contains(token))
+                {
+                    data.Remove(token); 
+                }
+
+
+                return Results.Ok("Se cerro la secion");
+            });
+        }
+    }
+}
---
+++ AAMG202309018/Endpoints/BodegaEndpoint.cs
@@ -0,0 +1,43 @@
+﻿namespace AAMG202309018.Endpoints
+{
+    public static class BodegaEndpoint
+    {
+        static List<object> data = new List<object>();
+        public static void AddBodegaEndpoints(this WebApplication app)
+        {
+            app.MapPost("/bodega", (string id, string producto) =>
+            {
+                data.Add(new { id, producto });
+                return Results.Ok();
+            }).AllowAnonymous();
+
+            app.MapGet("/bodega/{id}", (string id) =>
+            {
+                var item = data.FirstOrDefault(x => x.GetType().GetProperty("id")?.GetValue(x)?.ToString() == id);
+                if (item != null)
+                {
+                    return Results.Ok(item);
+                }
+                else
+                {
+                    return Results.NotFound();
+                }
+            }).AllowAnonymous();
+
+            app.MapPut("/bodega/{id}", (string id, string producto) =>
+            {
+                var item = data.FirstOrDefault(x => x.GetType().GetProperty("id")?.GetValue(x)?.ToString() == id);
+                if (item != null)
+                {
+
+                    item.GetType().GetProperty("producto")?.SetValue(item, producto);
+                    return Results.Ok();
+                }
+                else
+                {
+                    return Results.NotFound();
+                }
+            }).AllowAnonymous();
+        }
+    }
+}
